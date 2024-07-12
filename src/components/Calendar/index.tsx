79b9e3619730d94696b472fbbbb0a105665650b1
Gustavo Silva de Oliveira <gustavosilva1994_@hotmail.com>
2024-07-12 15:06:12 -0300

    navegando nos meses

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,23 +1,44 @@
 import { CaretLeft, CaretRight } from "phosphor-react";
 import { CalendarActions, CalendarBody, CalendarContainer, CalendarDay, CalendarHeader, CalendarTitle, } from "./styles";
 import { getWeekDays } from '../../utils/get-week-days'
+import { useState } from "react";
+import dayjs from "dayjs";
 
 export function Calendar() {
+    const [currentDate, setCurrentDate] = useState(() => {
+        return dayjs().set('date', 1)
+    })
+
+    function handlePreviousMonth() {
+        const previousMonthDate = currentDate.subtract(1, 'month')
+        
+        setCurrentDate(previousMonthDate)
+    }
+
+    function handleNextMonth() {
+        const previousMonthDate = currentDate.add(1, 'month')
+        
+        setCurrentDate(previousMonthDate)
+    }
+
     const shortWeekDays = getWeekDays({ short: true })
+
+    const currentMonth = currentDate.format('MMMM')
+    const currentYear = currentDate.format('YYYY')
 
     return (
         <CalendarContainer>
             <CalendarHeader>
                 <CalendarTitle>
-                    Julho <span>2024</span>
+                    {currentMonth} <span>{currentYear}</span>
                 </CalendarTitle>
 
-                <CalendarActions>
+                <CalendarActions onClick={handlePreviousMonth} title="Previous month">
                     <button>
                         <CaretLeft />
                     </button>
 
-                    <button>
+                    <button onClick={handleNextMonth} title="Next month">
                         <CaretRight />
                     </button>
                 </CalendarActions>
