75dc5ed3f09dc9d396af33bd3e8d373f661fb40b
Gustavo Silva de Oliveira <gustavosilva1994_@hotmail.com>
2024-07-15 11:04:46 -0300

    tabela de agendamentos

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,11 +1,16 @@
 import { Calendar } from "@/src/components/Calendar";
 import { Container, TimePicker, TimePickerHeader, TimePickerItem, TimePickerList } from "./styles";
 import { useState } from "react";
+import dayjs from "dayjs";
 
 export function CalendarStep() {
     const [selectedDate, setSelectedDate] = useState<Date | null>(null)
-    
+
     const isDateSelected = !!selectedDate
+
+    const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null
+    const describedDate = selectedDate ? dayjs(selectedDate).format('DD[ de ]MMMM') : null
+
 
     return (
         <Container isTimePickerOpen={isDateSelected}>
@@ -14,7 +19,7 @@ export function CalendarStep() {
             {isDateSelected && (
                 <TimePicker>
                     <TimePickerHeader>
-                        quinta-feira <span>11 de julho</span>
+                        {weekDay} <span>{describedDate}o</span>
                     </TimePickerHeader>
 
                     <TimePickerList>
